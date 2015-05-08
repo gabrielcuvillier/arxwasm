79f5aaa49ddb86efa19692e5b5c233e49c6983a1
Gabriel Cuvillier <contact@gabrielcuvillier.fr>
2015-05-08 17:33:26 +0200

    Updates to buildsystem for pnacl

--- cmake/toolchain-pnacl.cmake
+++ cmake/toolchain-pnacl.cmake
@@ -5,22 +5,23 @@ set(PLATFORM_NAME	          "PNaCl")
 set(PLATFORM_C_COMPILER     "clang")				      
 set(PLATFORM_CXX_COMPILER   "clang++")			       
 set(PLATFORM_TOOLCHAIN	    "linux_pnacl")		  
-set(PLATFORM_ARCH           "pnacl")			
-set(PLATFORM_ARCH_INTERNAL  "le32-nacl")			
+set(PLATFORM_ARCH           "pnacl")		
+set(PLATFORM_ARCH_INTERNAL  "le32-nacl")
 set(PLATFORM_EXE_SUFFIX     ".pexe")				    
 set(PLATFORM_SDK_ROOT       "$ENV{NACL_SDK_ROOT}")
-set(PLATFORM_SDK_INCLUDEDIR "${PLATFORM_SDK_ROOT}/include")
-set(PLATFORM_SDK_LIBDIR     "${PLATFORM_SDK_ROOT}/lib/${PLATFORM_ARCH}/Release")
-set(PLATFORM_PREFIX         "${PLATFORM_SDK_ROOT}/toolchain/${PLATFORM_TOOLCHAIN}")
+set(PLATFORM_SDK_INC_DIR    "${PLATFORM_SDK_ROOT}/include")
+set(PLATFORM_SDK_LIB_DIR    "${PLATFORM_SDK_ROOT}/lib/${PLATFORM_ARCH}/Release")
+set(PLATFORM_TOOLCHAIN_DIR  "${PLATFORM_SDK_ROOT}/toolchain/${PLATFORM_TOOLCHAIN}")
+set(PLATFORM_ROOT_DIR       "${PLATFORM_TOOLCHAIN_DIR}/${PLATFORM_ARCH_INTERNAL}")
 
 set(CMAKE_SYSTEM_NAME       "Linux")
-set(CMAKE_SYSTEM_PROCESSOR  "LLVM-IR")
-set(CMAKE_FIND_ROOT_PATH 	  "${PLATFORM_PREFIX}/${PLATFORM_ARCH_INTERNAL}" )
-set(CMAKE_AR                "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-ar")
-set(CMAKE_RANLIB            "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-ranlib")
-set(CMAKE_STRIP             "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-strip")
-set(CMAKE_C_COMPILER        "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-${PLATFORM_C_COMPILER}")
-set(CMAKE_CXX_COMPILER      "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-${PLATFORM_CXX_COMPILER}")
+set(CMAKE_SYSTEM_PROCESSOR  "${PLATFORM_ARCH_INTERNAL}")
+set(CMAKE_FIND_ROOT_PATH 	  "${PLATFORM_ROOT_DIR}" )
+set(CMAKE_AR                "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-ar")
+set(CMAKE_RANLIB            "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-ranlib")
+set(CMAKE_STRIP             "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-strip")
+set(CMAKE_C_COMPILER        "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-${PLATFORM_C_COMPILER}")
+set(CMAKE_CXX_COMPILER      "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-${PLATFORM_CXX_COMPILER}")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
@@ -28,28 +29,31 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 # Directories
-
-include_directories(${PLATFORM_SDK_INCLUDEDIR})
+include_directories(${PLATFORM_SDK_INC_DIR})
 
 # Force include directory of FreeType on NACL (it is not clear why FreeType is not detected without this)
-set(FREETYPE_INCLUDE_DIRS	${PLATFORM_PREFIX}/${PLATFORM_ARCH_INTERNAL}/usr/include/freetype2)
+set(FREETYPE_INCLUDE_DIRS	${PLATFORM_ROOT_DIR}/usr/include/freetype2)
 
 # Regal library is used as a wrapper to OPENGL ES 2
-set(OPENGL_INCLUDE_DIR 		${PLATFORM_PREFIX}/${PLATFORM_ARCH_INTERNAL}/usr/include)
-set(OPENGL_gl_LIBRARY    	${PLATFORM_PREFIX}/${PLATFORM_ARCH_INTERNAL}/usr/lib/libRegal.a)
-set(OPENGL_LIBRARIES	    ${PLATFORM_PREFIX}/${PLATFORM_ARCH_INTERNAL}/usr/lib/libglslopt.a) # this is required too
+set(OPENGL_INCLUDE_DIR 		${PLATFORM_ROOT_DIR}/usr/include)
+set(OPENGL_gl_LIBRARY    	${PLATFORM_ROOT_DIR}/usr/lib/libRegal.a)
+set(OPENGL_LIBRARIES	    ${PLATFORM_ROOT_DIR}/usr/lib/libglslopt.a) # this is required too
 
 # PPAPI libraries
-set(PPAPI_SIMPLE_LIBRARY  "-Wl,-undefined=PSUserCreateInstance" "${PLATFORM_SDK_LIBDIR}/libppapi_simple.a")
-set(NACL_IO_LIBRARY       "${PLATFORM_SDK_LIBDIR}/libnacl_io.a")
-set(PPAPI_GLES2_LIBRARY   "${PLATFORM_SDK_LIBDIR}/libppapi_gles2.a")
-set(PPAPI_CPP_LIBRARY     "${PLATFORM_SDK_LIBDIR}/libppapi_cpp.a")
+set(PPAPI_SIMPLE_LIBRARY  "-Wl,-undefined=PSUserCreateInstance" "${PLATFORM_SDK_LIB_DIR}/libppapi_simple.a")
+set(NACL_IO_LIBRARY       "${PLATFORM_SDK_LIB_DIR}/libnacl_io.a")
+set(PPAPI_GLES2_LIBRARY   "${PLATFORM_SDK_LIB_DIR}/libppapi_gles2.a")
+set(PPAPI_CPP_LIBRARY     "${PLATFORM_SDK_LIB_DIR}/libppapi_cpp.a")
 set(PPAPI_LIBRARY         "-lppapi")
 
 macro(pnacl_finalize target)
   add_custom_command(TARGET ${target} POST_BUILD
-    COMMENT "Finalizing ${target}"
-    COMMAND "cp" "$<TARGET_FILE:${target}>" "$<TARGET_FILE:${target}>.bc"
-    COMMAND "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-finalize" "$<TARGET_FILE:${target}>")
+    COMMENT "Finalizing ${target} to ${target}.pexe"
+    COMMAND "cp" "$<TARGET_FILE:${target}>" "$<TARGET_FILE:${target}>_unstripped.bc"
+    COMMAND "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-finalize" "$<TARGET_FILE:${target}>" "-o"  "$<TARGET_FILE:${target}>${PLATFORM_EXE_SUFFIX}")
 endmacro()
 
+macro(pnacl_compress target)
+  add_custom_target(pnacl-compress
+    COMMAND "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-compress" "$<TARGET_FILE:${target}>${PLATFORM_EXE_SUFFIX}")
+endmacro()
--- cmake/toolchain-x86_64-nacl.cmake
+++ cmake/toolchain-x86_64-nacl.cmake
@@ -1,5 +1,6 @@
 set(NACL 1)
 
+
 set(PLATFORM_NAME	          "NaCl")				     
 set(PLATFORM_C_COMPILER     "gcc")				      
 set(PLATFORM_CXX_COMPILER   "g++")			       
@@ -7,18 +8,19 @@ set(PLATFORM_TOOLCHAIN	    "linux_x86_newlib")
 set(PLATFORM_ARCH           "x86_64-nacl")			
 set(PLATFORM_EXE_SUFFIX     ".nexe")				    
 set(PLATFORM_SDK_ROOT       "$ENV{NACL_SDK_ROOT}")
-set(PLATFORM_SDK_INCLUDEDIR "${PLATFORM_SDK_ROOT}/include")
-set(PLATFORM_SDK_LIBDIR     "${PLATFORM_SDK_ROOT}/lib/newlib_x86_64/Release")
-set(PLATFORM_PREFIX         "${PLATFORM_SDK_ROOT}/toolchain/${PLATFORM_TOOLCHAIN}")
+set(PLATFORM_SDK_INC_DIR    "${PLATFORM_SDK_ROOT}/include")
+set(PLATFORM_SDK_LIB_DIR    "${PLATFORM_SDK_ROOT}/lib/newlib_x86_64/Release")
+set(PLATFORM_TOOLCHAIN_DIR  "${PLATFORM_SDK_ROOT}/toolchain/${PLATFORM_TOOLCHAIN}")
+set(PLATFORM_ROOT_DIR       "${PLATFORM_TOOLCHAIN_DIR}/${PLATFORM_ARCH}")
 
 set(CMAKE_SYSTEM_NAME       "Linux")
 set(CMAKE_SYSTEM_PROCESSOR  "x86_64")
-set(CMAKE_FIND_ROOT_PATH 	  "${PLATFORM_PREFIX}/${PLATFORM_ARCH}" )
-set(CMAKE_AR                "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-ar")
-set(CMAKE_RANLIB            "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-ranlib")
-set(CMAKE_STRIP             "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-strip")
-set(CMAKE_C_COMPILER        "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-${PLATFORM_C_COMPILER}")
-set(CMAKE_CXX_COMPILER      "${PLATFORM_PREFIX}/bin/${PLATFORM_ARCH}-${PLATFORM_CXX_COMPILER}")
+set(CMAKE_FIND_ROOT_PATH 	  "${PLATFORM_ROOT_DIR}" )
+set(CMAKE_AR                "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-ar")
+set(CMAKE_RANLIB            "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-ranlib")
+set(CMAKE_STRIP             "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-strip")
+set(CMAKE_C_COMPILER        "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-${PLATFORM_C_COMPILER}")
+set(CMAKE_CXX_COMPILER      "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-${PLATFORM_CXX_COMPILER}")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
@@ -26,22 +28,26 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 # Directories
-
-include_directories(${PLATFORM_SDK_INCLUDEDIR})
+include_directories(${PLATFORM_SDK_INC_DIR})
 
 # Force include directory of FreeType on NACL (it is not clear why FreeType is not detected without this)
-set(FREETYPE_INCLUDE_DIRS	${PLATFORM_PREFIX}/${PLATFORM_ARCH}/usr/include/freetype2)
+set(FREETYPE_INCLUDE_DIRS	${PLATFORM_ROOT_DIR}/usr/include/freetype2)
 
 # Regal library is used as a wrapper to OPENGL ES 2
-set(OPENGL_INCLUDE_DIR 		${PLATFORM_PREFIX}/${PLATFORM_ARCH}/usr/include)
-set(OPENGL_gl_LIBRARY    	${PLATFORM_PREFIX}/${PLATFORM_ARCH}/usr/lib/libRegal.a)
-set(OPENGL_LIBRARIES	    ${PLATFORM_PREFIX}/${PLATFORM_ARCH}/usr/lib/libglslopt.a) # this is required too
+set(OPENGL_INCLUDE_DIR 		${PLATFORM_ROOT_DIR}/usr/include)
+set(OPENGL_gl_LIBRARY    	${PLATFORM_ROOT_DIR}/usr/lib/libRegal.a)
+set(OPENGL_LIBRARIES	    ${PLATFORM_ROOT_DIR}/usr/lib/libglslopt.a) # this is required too
 
 # PPAPI libraries
-set(PPAPI_SIMPLE_LIBRARY  "-Wl,-undefined=PSUserCreateInstance" "${PLATFORM_SDK_LIBDIR}/libppapi_simple.a")
-set(NACL_IO_LIBRARY       "${PLATFORM_SDK_LIBDIR}/libnacl_io.a")
-set(PPAPI_GLES2_LIBRARY   "${PLATFORM_SDK_LIBDIR}/libppapi_gles2.a")
-set(PPAPI_CPP_LIBRARY     "${PLATFORM_SDK_LIBDIR}/libppapi_cpp.a")
+set(PPAPI_SIMPLE_LIBRARY  "-Wl,-undefined=PSUserCreateInstance" "${PLATFORM_SDK_LIB_DIR}/libppapi_simple.a")
+set(NACL_IO_LIBRARY       "${PLATFORM_SDK_LIB_DIR}/libnacl_io.a")
+set(PPAPI_GLES2_LIBRARY   "${PLATFORM_SDK_LIB_DIR}/libppapi_gles2.a")
+set(PPAPI_CPP_LIBRARY     "${PLATFORM_SDK_LIB_DIR}/libppapi_cpp.a")
 set(PPAPI_LIBRARY         "-lppapi")
 
- 
+macro(nacl_strip target)
+  add_custom_command(TARGET ${target} POST_BUILD
+    COMMENT "Stripping ${target} to ${target}_${CMAKE_SYSTEM_PROCESSOR}${PLATFORM_EXE_SUFFIX}"
+    COMMAND "cp" "$<TARGET_FILE:${target}>" "$<TARGET_FILE:${target}>_${CMAKE_SYSTEM_PROCESSOR}_unstripped${PLATFORM_EXE_SUFFIX}"
+    COMMAND "${PLATFORM_TOOLCHAIN_DIR}/bin/${PLATFORM_ARCH}-strip" "$<TARGET_FILE:${target}>" "-o" "$<TARGET_FILE:${target}>_${CMAKE_SYSTEM_PROCESSOR}${PLATFORM_EXE_SUFFIX}")
+endmacro()
