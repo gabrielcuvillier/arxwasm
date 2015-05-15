a3a87b7767ce5b577783e4939a12967f45c9fb78
Gabriel Cuvillier <contact@gabrielcuvillier.fr>
2015-05-15 19:01:25 +0200

    Reworked PNACL javascript loader

--- data/nacl/www-root/arx.js
+++ data/nacl/www-root/arx.js
@@ -10,59 +10,77 @@ function updateStatus(message) {
   document.getElementById(STATUS_ID).innerHTML += message + '<br/>';  // STATUS_ID defined by html file
 }
 
+function enableArxLaunch() {
+  document.getElementById(LAUNCH_ARX_ID).innerHTML = 'Click "Launch" button to start game';
+  document.getElementById(LAUNCH_ARX_ID).innerHTML += '<br/><input type="button" id="launch" value="Launch Arx">';
+  document.getElementById('launch').addEventListener( 'click',
+                                                      runArx,
+                                                      false);       
+}
+
 function runArx() {
-  var embed = document.createElement('embed');
-  // MODULE_* variables must be defined by the main html file
-  embed.width = MODULE_WIDTH;
-  embed.height = MODULE_HEIGHT;
-  embed.type = MODULE_MIMETYPE;
-  embed.src = MODULE_MANIFEST;
-  embed.setAttribute('PS_STDOUT', "dev/console1");
-  embed.setAttribute('PS_STDERR', "dev/console1");
-  document.getElementById(MAIN_ID).appendChild(embed);   // MAIN_ID defined by html file
-  
-  function log_handler(tag) {
-    return function () {
-      var args = [tag].concat(Array.prototype.slice.call(arguments));
-      console.log.apply(console, args);
-    }
-  }
-
-  function error_handler() {
-    console.error("Error occurred:", embed.lastError);
-  }
-  
-  function crash_handler() {
-    console.log("Exit code:", embed.exitStatus);
-  }
-
-  function progress_handler(evt) {
-    if (evt.total != 0) {
-      var progress = Number((evt.loaded * 100 / evt.total).toFixed(0));
-      document.getElementById(LOADING_STATUS_ID).innerHTML = "Loading: " + progress + "%";  
-
-      if (progress > 94) {
-        document.getElementById(LOADING_STATUS_ID).innerHTML += "<br/>" + "Please, wait some more time while approaching 100%...";
+  document.getElementById(SELECT_DATA_ID).innerHTML = '';
+  document.getElementById(LAUNCH_ARX_ID).innerHTML = '';
+  updateStatus("Requesting persistent storage (for saves, config files, etc.).");
+  window.webkitRequestFileSystem(window.PERSISTENT, 15 * 1024 * 1024, 
+    function(fs) {
+      updateStatus("Launching Arx.");
+      var embed = document.createElement('embed');
+      // MODULE_* defined by the main html file
+      embed.width = MODULE_WIDTH;
+      embed.height = MODULE_HEIGHT;
+      embed.type = MODULE_MIMETYPE;
+      embed.src = MODULE_MANIFEST;
+      embed.setAttribute('PS_STDOUT', "dev/console1");
+      embed.setAttribute('PS_STDERR', "dev/console1");
+      document.getElementById(MAIN_ID).appendChild(embed);   // MAIN_ID defined by html file
+      
+      function log_handler(tag) {
+        return function () {
+          var args = [tag].concat(Array.prototype.slice.call(arguments));
+          console.log.apply(console, args);
+        }
       }
-    }
-  }
 
-  function load_handler(evt) {
-    document.getElementById(LOADING_STATUS_ID).innerHTML = "Loading: success.";
-  }
+      function error_handler() {
+        console.error("Error occurred:", embed.lastError);
+      }
+      
+      function crash_handler() {
+        console.log("Exit code:", embed.exitStatus);
+      }
 
-  embed.addEventListener('loadstart', log_handler("loadstart"));
-  embed.addEventListener('progress', log_handler("progress"));
-  embed.addEventListener('progress', progress_handler);
-  embed.addEventListener('load', log_handler("load"));
-  embed.addEventListener('load', load_handler);
-  embed.addEventListener('error', log_handler("error"));
-  embed.addEventListener('abort', log_handler("abort"));
-  embed.addEventListener('loadend', log_handler("loadend"));
-  embed.addEventListener('message', log_handler("message"));
-  embed.addEventListener('error', error_handler);
-  embed.addEventListener('abort', error_handler);
-  embed.addEventListener('crash', crash_handler);
+      function progress_handler(evt) {
+        if (evt.total != 0) {
+          var progress = Number((evt.loaded * 100 / evt.total).toFixed(0));
+          document.getElementById(LOAD_STATUS_ID).innerHTML = "Loading: " + progress + "%";  
+
+          if (progress > 94) {
+            document.getElementById(LOAD_STATUS_ID).innerHTML += "<br/>" + "Please, wait some more time while approaching 100%...";
+          }
+        }
+      }
+
+      function load_handler(evt) {
+        document.getElementById(LOAD_STATUS_ID).innerHTML = "Loading: success.";
+      }
+
+      embed.addEventListener('loadstart', log_handler("loadstart"));
+      embed.addEventListener('progress', log_handler("progress"));
+      embed.addEventListener('progress', progress_handler);
+      embed.addEventListener('load', log_handler("load"));
+      embed.addEventListener('load', load_handler);
+      embed.addEventListener('error', log_handler("error"));
+      embed.addEventListener('abort', log_handler("abort"));
+      embed.addEventListener('loadend', log_handler("loadend"));
+      embed.addEventListener('message', log_handler("message"));
+      embed.addEventListener('error', error_handler);
+      embed.addEventListener('abort', error_handler);
+      embed.addEventListener('crash', crash_handler);
+    },
+    function(fs) {
+      console.log("Unable to get persistent storage.");
+    });
 }
 
 function uploadDidChange(event) {
@@ -103,7 +121,12 @@ function uploadDidChange(event) {
             dirEntry.getFile(f.name, {create: true}, function(fileEntry) {
               fileEntry.createWriter(function(fileWriter) {
                 function done(evt) {
-                  updateStatus(f.name + ' copied to html5 filesystem.');                
+                  updateStatus(f.name + ' copied to html5 filesystem.');        
+
+                  if (f.name === PAK_FILE) {
+                    updateStatus("Found Arx data in local html5 filesystem.");
+                    enableArxLaunch();
+                  }        
                 };
                 function error(evt) {
                   updateStatus('Error while copying ' + f.name);
@@ -119,44 +142,38 @@ function uploadDidChange(event) {
 }
 
 function loadFromLocalStorage() {
-  updateStatus('Searching for Arx data in local html5 filesystem (' + ARX_DIR + '/' + PAK_FILE + ').');
+  updateStatus('Searching for Arx data in local html5 filesystem (temporary: /' + ARX_DIR + '/' + PAK_FILE + ').');
   
   function loadSuccess() {
-    updateStatus('Found Arx data in local html5 filesystem. Launching arx.');
-    runArx();
+    updateStatus('Found Arx data in local html5 filesystem.');
+    enableArxLaunch();
   }
 
   function loadFailure() {
     updateStatus('Arx data not found in local html5 filesystem.');
     updateStatus('Select game data (all PAK files and cfg_default.ini) to copy to local html5 filesystem.');
-    updateStatus('');
-    document.getElementById(MAIN_ID).innerHTML =
-      '<input type="file" id="infile" multiple> \
-       <input type="button" id="launch" value="Launch Arx">';
-    document.getElementById('infile').addEventListener('change',
-                                                       uploadDidChange,
-                                                       false);
-    document.getElementById('launch').addEventListener('click',
-                                                       loadSuccess,
-                                                       false);                                                       
+    document.getElementById(SELECT_DATA_ID).innerHTML = '<input type="file" id="infile" multiple>';
+    document.getElementById('infile').addEventListener( 'change',
+                                                        uploadDidChange,
+                                                        false);        
   }
 
   window.webkitRequestFileSystem(window.TEMPORARY, 1024 * 1024 * 1024,
     function(fs) {
       fs.root.getDirectory(ARX_DIR, {}, function(dirEntry) {
-        dirEntry.getFile(PAK_FILE, {}, loadSuccess, loadFailure) 
+        dirEntry.getFile(PAK_FILE, {}, loadSuccess, loadFailure);
       }, loadFailure);
     }
   );
 }
 
 window.onload = function() {
-  updateStatus('Searching for Arx data hosted on server (' + ARX_DIR + '/' + PAK_FILE + ').');
+  updateStatus('Searching for Arx data hosted on server (URL: ./' + ARX_DIR + '/' + PAK_FILE + ').');
   var req = new XMLHttpRequest();
   req.onload = function() {
     if (this.status === 200) {
-      updateStatus('Found Arx data on server. Launching arx.');
-      runArx();
+      updateStatus('Found Arx data on server.');
+      enableArxLaunch();
     } else {
       updateStatus('Arx data not found on server.');
       loadFromLocalStorage();
